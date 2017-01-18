1d5387cf6fc8f7a29779b899475b21e2be506940
ekinrf <ekinrf@gmail.com>
2017-01-18 23:28:56 +0000

    Correct Python Memo implementation

--- LongestIncreasingSubseq/python/longest_inc_subseq.py
+++ LongestIncreasingSubseq/python/longest_inc_subseq.py
@@ -1,17 +1,34 @@
 def longest_inc_subseq(array):
+    class Memo:
+        def __init__(self, f):
+            self.f = f
+            self.cache = {}
+
+        def get(self, i):
+            if i in self.cache:
+                return self.cache[i]
+            else:
+                self.cache[i] = self.f(i)
+                return self.cache[i]
+
     def lis_ending_at_index(i):
         if i is 0:
             return 1, i  # length, index
         else:
-            lis_list = [lis_ending_at_index(index) for index in range(0, i) if array[index] < array[i]]
+            lis_list = [memo.get(index) for index in range(0, i) if array[index] < array[i]]
+            # lis_list = [lis_ending_at_index(index) for index in range(0, i) if array[index] < array[i]]
+
             if lis_list:
                 length, index = max(lis_list, key=lambda x: x[0])
                 return length + 1, i
             else:
                 return 1, i
 
-    lis_list = [lis_ending_at_index(x) for x in range(0, len(array))]
-    return max(lis_list, key=lambda x: x[0])
+    memo = Memo(lis_ending_at_index)
+    lis_list = [memo.get(x) for x in range(0, len(array))]
+    # lis_list = [lis_ending_at_index(x) for x in range(0, len(array))]
+
+    return max(lis_list, key=lambda x: x[0])[0]
 
 
 def lis_quick_search(array):
@@ -35,13 +52,14 @@ def lis_quick_search(array):
 
 import timeit
 
+
 print(longest_inc_subseq([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]))
 print(lis_quick_search([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]))
-t1 = timeit.Timer("longest_inc_subseq([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15])",
+t1 = timeit.Timer("longest_inc_subseq(range(0, 100))",
                   "from __main__ import longest_inc_subseq")
-t2 = timeit.Timer("lis_quick_search([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15])",
+t2 = timeit.Timer("lis_quick_search(range(0, 100))",
                   "from __main__ import lis_quick_search")
 
-print(t1.timeit(1000))
-print(t2.timeit(1000))
+print(t1.timeit(20))
+print(t2.timeit(20))
 
--- StringEditDistance/Python/StringEditDistance.py
+++ StringEditDistance/Python/StringEditDistance.py
@@ -1,11 +1,12 @@
-def memo(x, y, f):
-    cache = {}
-
+def memo(x, y, f, cache):
     def query():
         if (x, y) not in cache:
             cache[(x, y)] = f(x, y)
         return cache[(x, y)]
     return query()
+
+
+cache = {}
 
 
 def cal_edit_distance(ori, tar):
@@ -18,14 +19,15 @@ def cal_edit_distance(ori, tar):
     if not tar:
         return len(ori), "remove %s" % ori
     ori_head, ori_rest, tar_head, tar_rest = ori[0], ori[1:], tar[0], tar[1:]
-    edit_op_dis = memo(ori_rest, tar_rest, cal_edit_distance)
+    edit_op_dis = memo(ori_rest, tar_rest, cal_edit_distance, cache)
     if ori_head != tar_head:
         edit_op_dis = edit_tuple(edit_op_dis, 1, "replace %s with %s" % (ori_head, tar_head))
-    del_op_dis = memo(ori_rest, tar, cal_edit_distance)
+    del_op_dis = memo(ori_rest, tar, cal_edit_distance, cache)
     del_op_dis = edit_tuple(del_op_dis, 1, "delete %s" % ori_head)
-    add_op_dis = memo(ori, tar_rest, cal_edit_distance)
+    add_op_dis = memo(ori, tar_rest, cal_edit_distance, cache)
     add_op_dis = edit_tuple(add_op_dis, 1, "add %s" % tar_head)
     return min(edit_op_dis, del_op_dis, add_op_dis, key=lambda e: e[0])
 
-print(cal_edit_distance("kitten", "sitting")[1])
+
+print(cal_edit_distance("book", "bokop")[1])
 
