a566e3bb5f7d4218c04b81e83ddbbf1b4b5b3311
ekinrf <ekinrf@gmail.com>
2017-06-29 22:15:29 +0100

    eval_div by floyd-warshall and disjoint set. longest subarray sum less than k for both positives and negatives.

--- EvalDiv/python/eval_div.py
+++ EvalDiv/python/eval_div.py
@@ -1,14 +1,77 @@
+# path finding in a graph
+
+from collections import defaultdict
+
+
+# the floyd warshall algorithm
+# unlike floyd-warshall who finds a shortest path,
+# this simply finds a path
 def eval_div(equations, values, queries):
-    results = []
+    eq_graph = defaultdict(dict)
+    for (dividend, divisor), value in zip(equations, values):
+        eq_graph[dividend][divisor] = value
+        eq_graph[divisor][dividend] = 1 / value
+        eq_graph[dividend][dividend] = eq_graph[divisor][divisor] = 1
+    for k in eq_graph:
+        for i in eq_graph[k]:
+            for j in eq_graph[k]:
+                eq_graph[i][j] = eq_graph[i][k] * eq_graph[k][j]
 
-    for (dividend, divisor) in queries:
-        if divisor is '0':
-            results.append(-1.0)
+    for dividend, divisor in queries:
+        yield eq_graph[dividend].get(divisor, -1.0)
+
+
+# an union-find solution
+# find if dividend and divisor are in the same set
+# close to O(n) depends on the find_root function
+# optimise by compact and put a smaller tree under bigger tree when union
+def eval_div_union(equations, values, queries):
+    # compaction can be done here
+    def find_root(vtx, weight):
+        parent, weight = union_find_tree[vtx][0], union_find_tree[vtx][1] * weight
+        if parent is vtx:
+            return vtx, weight
         else:
-            try:
-                dividend = float(dividend)
-                divsior = float(divisor)
-                divsior
+            return find_root(parent, weight)
 
-            except ValueError:
-                results.append(-1.0)
+    union_find_tree = {}
+    for (dividend, divisor), value in zip(equations, values):
+        # both vtx not in the set yet, choose one to a root
+        if dividend not in union_find_tree and divisor not in union_find_tree:
+            union_find_tree[dividend] = (dividend, 1)
+            union_find_tree[divisor] = (dividend, value)
+        elif dividend not in union_find_tree:
+            union_find_tree[dividend] = (divisor, 1 / value)
+        elif divisor not in union_find_tree:
+            union_find_tree[divisor] = (dividend, value)
+        else:  # both are in the set, try to perform an union
+            dividend_root, dividend_root_weight = find_root(dividend, 1)
+            divisor_root, divisor_root_weight = find_root(divisor, 1)
+            if divisor_root is not dividend_root:  # not connected
+                # who wants be root? traditional union find would decide by comparing the depth of each tree
+                union_find_tree[divisor_root] = (dividend_root, value * dividend_root_weight / divisor_root_weight)
+    for dividend, divisor in queries:
+        if divisor not in union_find_tree or dividend not in union_find_tree:
+            yield -1.0
+        else:
+            dividend_root, dividend_root_weight = find_root(dividend, 1)
+            divisor_root, divisor_root_weight = find_root(divisor, 1)
+            if divisor_root is not dividend_root:
+                yield -1.0
+            else:
+                yield divisor_root_weight / dividend_root_weight
+
+
+if __name__ == '__main__':
+    equations = [["a", "b"], ["b", "c"]]
+    values = [2.0, 3.0]
+    queries = [["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"]]
+
+    eq1 = [["a", "b"], ["e", "f"], ["b", "e"]]
+    v1 = [3.4, 1.4, 2.3]
+    q1 = [["b", "a"], ["a", "f"], ["f", "f"], ["e", "e"], ["c", "c"], ["a", "c"], ["f", "e"]]
+
+    print(list(eval_div(equations, values, queries)))
+    print(list(eval_div_union(equations, values, queries)))
+    print(list(eval_div(eq1, v1, q1)))
+    print(list(eval_div_union(eq1, v1, q1)))
--- SubArray/python/longest_less_or_eq.py
+++ SubArray/python/longest_less_or_eq.py
@@ -1,4 +1,7 @@
-# all numbers in array are no less than 0:  time complexity O(n)
+
+
+# all numbers in array are no less than 0
+# O(n)
 def sub_array_less_or_eq_positive(array, target):
     start = end = max_start = max_len = sum = 0
 
@@ -25,8 +28,30 @@ def sub_array_less_or_eq_positive(array, target):
 
 
 # array contains any numbers negative or positive
+# O(n*logn)
 def sub_array_less_or_eq(array, target):
-    pass
+    import bisect
+
+    sum = [array[0]] * len(array)
+    max_sum = [array[0]] * len(array)
+    max_len, max_ending = 0, -1
+    for i in range(1, len(array)):
+        sum[i] = sum[i - 1] + array[i]
+        max_sum[i] = max(max_sum[i - 1], max_sum[i - 1] + array[i])
+        prev_target = sum[i] - target
+        lt_eq_prev_idx = bisect.bisect_left(max_sum, prev_target, 0, i)
+        if lt_eq_prev_idx is 0 and array[0] is not prev_target:
+            # the key is to find a max_sum[k] < prev_target, then k + 1 will be the start
+            # hence the special case at index 0
+            lt_eq_prev_idx += 1
+        if i - lt_eq_prev_idx + 1 > max_len:
+            max_len = i - lt_eq_prev_idx + 1
+            max_ending = i
+    return max_ending - max_len + 1, max_ending, max_len
 
 
-print(sub_array_less_or_eq_positive([3, 1, 2, 1], 4))
+print(sub_array_less_or_eq_positive([3, 3, 2, 1], 7))
+print(sub_array_less_or_eq_positive([1, 2, 3, 4], 8))
+
+print(sub_array_less_or_eq([3, 5, -3, 3, 2, 0], 7))
+print(sub_array_less_or_eq([3, 3, 2, 1], 7))
